d511e31d521e6f3c20cdd8a934409d20cdb1f251
BlancasJ <Jorge.Blancas@theksquaregroup.com>
2021-03-29 18:30:29 -0600

    try 2

--- my_resume/src/App.js
+++ my_resume/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 id="myName">Jorge Blancas Cruz</h1>
-        <p id="myInfo">Graduated from Computational Embedded Systems engineering. An enthusiast when it comes to technology and innovation, who always tries to think of a way to solve the problems that may arise. Not giving up easily, keeping on going until the goal has met. Nevertheless, a flexible person who can change the strategies according to the needs.
-        Lala</p>
+        <p id="myInfo">Graduated from Computational Embedded Systems engineering. An enthusiast when it comes to technology and innovation, who always tries to think of a way to solve the problems that may arise. Not giving up easily, keeping on going until the goal has met. Nevertheless, a flexible person who can change the strategies according to the needs.Lala</p>
       </header>
     </div>
   );
